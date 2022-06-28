2402415c09a7fea328b006ec655c65214f578c73
gift56 <efeasiughu@gmail.com>
2022-06-28 14:33:59 +0100

    stying some area

--- src/component/transaction/Transaction.jsx
+++ src/component/transaction/Transaction.jsx
@@ -34,17 +34,31 @@ const Transaction = () => {
                         <h3>Date</h3>
                         <h3>Priority</h3>
                     </div>
-                    {/* <div className="tableData">
-                            <div className="data">
-                                <div className="dataProfile">
-                                    <img src={Images.user1} alt="" />
-                                    <div className="userName">
-                                        <h4>Contact Email not Linked</h4>
-                                        <p>Updated 1 day ago</p>
-                                    </div>
+                    <div className="tableData">
+                        <div className="data">
+                            <div className="dataProfile">
+                                <img src={Images.user1} alt="" />
+                                <div className="userUpdate">
+                                    <h4>Contact Email not Linked</h4>
+                                    <p>Updated 1 day ago</p>
                                 </div>
                             </div>
-                        </div> */}
+                            <div className="customerName">
+                                <h4>Tom Cruise</h4>
+                                <p>on 24.05.2019</p>
+                            </div>
+                            <div className="date">
+                                <h4>May 26, 2019</h4>
+                                <p>May 26, 2019</p>
+                            </div>
+                            <div className="priority">
+                                <button className='btn High'>High</button>
+                            </div>
+                            <div className="sideIcon">
+                                <img src={Images.sideMenu} alt="" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
