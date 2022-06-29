b934d32a8c4e144678135ec4e9d6788e08795080
gift56 <efeasiughu@gmail.com>
2022-06-29 14:31:40 +0100

    starting mobile view

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./component/sidebar/Sidebar";
 import Pages from "./component/pages/Pages";
 import './component/sass/styles.css'
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleMobileMenu = () => {
+    setIsMobile((prev) => !prev);
+  };
+
   return (
-    <div className="App">
+    <div className={` ${isMobile ? "openBars" : "App"}`}>
       <BrowserRouter>
-        <Sidebar />
+        <Sidebar isMobile={isMobile} handleMobileMenu={handleMobileMenu} />
         <Pages />
       </BrowserRouter>
     </div>
--- src/component/sidebar/Sidebar.jsx
+++ src/component/sidebar/Sidebar.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import { FaBars, FaTimes } from "react-icons/fa";
 import '../sass/styles.css'
 import sidebarContent from '../mapJs/Sidebar';
 import { Link } from 'react-router-dom';
 
-const Sidebar = () => {
+const Sidebar = ({ isMobile, handleMobileMenu }) => {
     document.title = 'Admin Dashboard';
+
     return (
         <nav className='sidebar'>
             {sidebarContent.map((content, index) => (
-                <div className='navBar' key={index}>
+                <div className={` ${isMobile ? "open" : "navBar"}`} key={index}>
                     <div className="navLogo">
                         <img src={content.title.image} alt="" />
                         <h4>{content.title.text}</h4>
                     </div>
-                    <ul className="navItems">
+                    <span className='bars' onClick={handleMobileMenu}>
+                        {isMobile ? <FaTimes /> : <FaBars />}
+                    </span>
+                    <ul className='navItems'>
                         {content.navOne.map((link, index) => (
                             <li className="navList" key={index}>
                                 <Link to={link.route}>
